aefdc85e250dd4b6fd88db8c3a96f022d998ff32
SamGreat <samantha@greatorex.org.uk>
2023-01-26 16:33:18 +0000

    removed unused code

--- api.js
+++ api.js
@@ -1,27 +1,5 @@
 const axios = require('axios');
 
-// exports.sendMessage = async function (roomName, switchOn) {
-//     console.log('Send Light Switch On Message');
-//     try {
-
-//      let messageData = {
-//         "action" : "ROOM_NATIVE_CONTROL",
-//         "data": {"roomName": roomName, "switchOn": switchOn}
-//         }
-//       let result = await axios({
-//         url: `https://6el4la4z4k.execute-api.eu-west-2.amazonaws.com/dev/ws/sendmessage`,
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         },
-//         data: JSON.stringify(messageData)
-//       });
-//       return result.data
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
 exports.sendMessage = async function (data) {
     try {
 
